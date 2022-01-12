b4d93111dd765ae44c7e4d720ef2dcb62a841676
asmakashif <94177460+asmakashif@users.noreply.github.com>
2022-01-12 21:52:53 +0530

    react-carousel commit

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import Carousel from "./components/Carousel/";
+import InfiniteCarousel from "./components/InfiniteCarousel/"
+
+const App = () => {
+    return (
+        <div style={{ maxWidth: 900, marginLeft: 'auto', marginRight: 'auto', marginTop: 64 }}>
+            <Carousel
+                show={4}
+            >
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+            </Carousel>
+
+            <InfiniteCarousel
+                show={4}
+                infiniteLoop
+            >
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+            </InfiniteCarousel>
+
+        </div>
+
+
+
+
+
+    )
+}
+
+export default App
